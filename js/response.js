a211f33825b9b86d530a31b7d63bdaaf1de11d5c
lzx0406 <zexuanli@andrew.cmu.edu>
2023-05-03 20:38:36 -0400

    added nav bars

---
+++ js/response.js
@@ -0,0 +1,28 @@
+// function storeValues() {
+//     var formData = {
+//       firstName: document.getElementById("species").value,
+//       lastName: document.getElementById("lastName").value,
+//       email: document.getElementById("email").value,
+//       password: document.getElementById("password").value
+//     };
+  
+//     console.log(formData);
+//   }
+
+// // function insertVal(value, id) {
+// //     id.innerHTML = value;
+// // }
+
+// function getValue(checkboxId) {
+//     var checkbox = document.getElementById(checkboxId);
+//     var isChecked = checkbox.checked;
+//     var value = isChecked ? checkbox.value : null;
+//     console.log(value + "here");
+//     // insertVal(value, checkboxId+"-r")
+//     //insertVal(value, "state-alive1-r")
+//     return value;
+//   }
+
+// const tmp = document.getElementById("state-alive1-r");
+// tmp.innerHTML = getValue("state-alive1");
+

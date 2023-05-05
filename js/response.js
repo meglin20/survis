88837d9f728aeaa2cd45e93e95703e50bbb389c6
lzx0406 <zexuanli@andrew.cmu.edu>
2023-05-05 14:40:42 -0400

    added dropbox

--- js/response.js
+++ js/response.js
@@ -1,3 +1,56 @@
+const dropArea = document.getElementById("drop-area");
+const fileList = document.getElementById("file-list");
+
+// Add event listeners
+["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+dropArea.addEventListener(eventName, preventDefaults, false);
+document.body.addEventListener(eventName, preventDefaults, false);
+});
+
+// Highlight drop area when item is dragged over it
+["dragenter", "dragover"].forEach(eventName => {
+dropArea.addEventListener(eventName, highlight, false);
+});
+
+// Unhighlight drop area when item is dragged out of it
+["dragleave", "drop"].forEach(eventName => {
+dropArea.addEventListener(eventName, unhighlight, false);
+      });
+
+// Handle dropped files
+dropArea.addEventListener("drop", handleDrop, false);
+
+function preventDefaults(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function highlight() {
+  dropArea.classList.add("dragging");
+}
+
+function unhighlight() {
+  dropArea.classList.remove("dragging");
+}
+
+function handleDrop(event) {
+  const files = event.dataTransfer.files;
+  validateFiles(files);
+  initializeProcess(files);
+}
+
+function validateFiles(files) {
+    // Add your validation logic here
+  }
+
+  function initializeProcess(files) {
+    for (const file of files) {
+      const listItem = document.createElement("li");
+      listItem.textContent = file.name;
+      fileList.appendChild(listItem);
+    }
+  }
+
 // function storeValues() {
 //     var formData = {
 //       firstName: document.getElementById("species").value,
@@ -25,4 +78,3 @@
 
 // const tmp = document.getElementById("state-alive1-r");
 // tmp.innerHTML = getValue("state-alive1");
-
